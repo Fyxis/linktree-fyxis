8468f585b04a778b692d5dada15e8436914b346d
Fyxis <bagusadrianus@gmail.com>
2024-12-30 10:26:02 +0700

    add new projects

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -7,7 +7,7 @@ const projects = [
     },
     {
         "id_project": 2,
-        "title": "Koperasi Anugerah Jaya Sejahtera",
+        "title": "AJS Cooperative Web",
         "description": "A project to develop a cooperative web application that facilitates member registration, financial management, and transaction records efficiently.",
         "site_link": "https://koperasi.smkn3kendal.sch.id"
     },
@@ -16,5 +16,11 @@ const projects = [
         "title": "Limitless RPG Discord Bot",
         "description": "Developed a Discord bot named Limitless RPG, featuring engaging RPG mechanics such as quests, battles, and item collection, providing an interactive gaming experience for users.",
         "site_link": "unknown"
+    },
+    {
+        "id_project": 4,
+        "title": "Portfolio Website",
+        "description": "A portfolio website is a personal online platform designed to showcase an individual's skills, work, and accomplishments. The purpose is to create a professional digital presence that highlights expertise and helps attract potential clients, employers, or collaborators.",
+        "site_link": "https://fyxis.github.io"
     }
 ]
